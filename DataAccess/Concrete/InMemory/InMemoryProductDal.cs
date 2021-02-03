df7b3c8c34e3ab8a69a0b17338e0aea2f94d84c5
Şükran Karakaya <sukran.karakaya@ogrenci.selcuk.edu.tr>
2021-02-04 01:42:34 +0300

    Add project files.

---
+++ DataAccess/Concrete/InMemory/InMemoryProductDal.cs
@@ -0,0 +1,74 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryProductDal : IProductDal
+    {
+        List<Product> _products;
+        public InMemoryProductDal()
+        {
+
+            _products = new List<Product>
+            {
+
+                new Product {ProductId=1, CategoryId =1, ProductName ="Bardak",UnitPrice=15, UnitsInStock=15},
+                new Product {ProductId=2, CategoryId =2, ProductName ="Kamera",UnitPrice=500, UnitsInStock=3},
+                new Product {ProductId=3, CategoryId =3, ProductName ="Telefon",UnitPrice=1500, UnitsInStock=2},
+                new Product {ProductId=4, CategoryId =4, ProductName ="Klavye",UnitPrice=150, UnitsInStock=65},
+                new Product {ProductId=5, CategoryId =5, ProductName ="Fare",UnitPrice=85, UnitsInStock=1},
+
+
+            };
+        }
+        public void Add(Product product)
+        {
+            _products.Add( product);
+        }
+
+        public void Delete(Product product)
+        {
+            Product productToDelete= _products.SingleOrDefault(p=>p.ProductId == product.ProductId);
+
+
+
+            _products.Remove(productToDelete);
+            
+        }
+
+        public Product Get(Expression<Func<Product, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAll()
+        {
+            return _products;
+        }
+
+        public List<Product> GetAll(Expression<Func<Product, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Product> GetAllByCategory(int catrgoryId)
+        {
+           return _products.Where(p => p.CategoryId == catrgoryId).ToList();
+        }
+
+        public void Update(Product product)
+        {
+            //Gönderdigim ürün ıd'sine ait olan listedeki ürünü bul.
+            Product productToDelete = _products.SingleOrDefault(p => p.ProductId == product.ProductId);
+            productToDelete.ProductName = product.ProductName;
+            productToDelete.ProductId = product.ProductId;
+            productToDelete.UnitPrice = product.UnitPrice;
+            productToDelete.UnitsInStock = product.UnitsInStock;
+        }
+    }
+}
